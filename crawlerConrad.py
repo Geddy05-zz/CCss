37e334b9dbc2cce2b0cf7de6d2c3a645ca25e064
Geddy05 <geddyschellevis@gmail.com>
2014-10-28 18:52:56 +0100

    added Coolblue crawler and fix Conrad crawler both working

--- crawlerConrad.py
+++ crawlerConrad.py
@@ -5,13 +5,17 @@ except:
 from bs4 import BeautifulSoup
 from lxml import html
 import re
-import py2neo
+import py2neo ,sys,re
 from py2neo import node, rel, neo4j
 
+
+#product urls Optische drives
 blurayBranders= "https://www.conrad.nl/nl/computer-kantoor/pc-componenten/drives-branders/interne-blu-ray-branders.html","Optischedrives"
 dvdBranders= "https://www.conrad.nl/nl/computer-kantoor/pc-componenten/drives-branders/interne-dvd-branders.html","Optischedrives"
 bluraySpeler = "https://www.conrad.nl/nl/computer-kantoor/pc-componenten/drives-branders/interne-blu-ray-spelers.html","Optischedrives"
 dvdSpeler = "https://www.conrad.nl/nl/computer-kantoor/pc-componenten/drives-branders/interne-dvd-spelers.html","Optischedrives"
+
+#product urls Harde schijven
 hddIdeSsd="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/harde-schijven/interne-harde-schijven/25-ide-ssd.html","Hardeschijf"
 hddSas ="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/harde-schijven/interne-harde-schijven/25-sas-hdd.html","Hardeschijf"
 HddSata="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/harde-schijven/interne-harde-schijven/25-sata-hdd.html","Hardeschijf"
@@ -20,85 +24,109 @@ hddSsh="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/harde-schijven/
 hddSasdrie="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/harde-schijven/interne-harde-schijven/35-sas-hdd.html","Hardeschijf"
 hddSataDrie="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/harde-schijven/interne-harde-schijven/35-sata-hdd.html","Hardeschijf"
 hddSshdDrie= "https://www.conrad.nl/nl/computer-kantoor/pc-componenten/harde-schijven/interne-harde-schijven/35-sshd-hdd.html","Hardeschijf"
+
+#product urls werk geheugen
 ddrone="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/intern-geheugen/ddr1-geheugen.html","Geheugen"
 ddrTwo="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/intern-geheugen/ddr2-geheugen.html","Geheugen"
 ddrThree="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/intern-geheugen/ddr3-geheugen.html","Geheugen"
 sdRam= "https://www.conrad.nl/nl/computer-kantoor/pc-componenten/intern-geheugen/sd-ram-geheugen.html","Geheugen"
-processorAMD = "https://www.conrad.nl/nl/computer-kantoor/pc-componenten/processoren/amd-socket-am3-cpus.html","processor"
-mbMiniITX="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/moederborden/mini-itx-moederborden.html","moederbord"
-mbSocket1150="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/moederborden/socket-1150-moederborden.html","moederbord"
-mbSocket1155="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/moederborden/socket-1155-moederborden.html","moederbord"
-mbSocket2011="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/moederborden/socket-2011-intel-moederborden.html","moederbord"
-mbSocket478="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/moederborden/socket-478-moederborden.html","moederbord"
-mbSocketAM="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/moederborden/socket-am3-amd-moederborden.html","moederbord"
-mbSocketFM="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/moederborden/socket-fm2-amd-moederborden.html","moederbord"
-geluidskaart="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/multimedia-hardware/geluidskaarten-intern.html","geluidskaart"
-behuizing="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/pc-behuizingen/pc-behuizingen.html","behuizing"
-processorAMD = "https://www.conrad.nl/nl/computer-kantoor/pc-componenten/processoren/amd-socket-am3-cpus.html","processor"
-processor1155="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/processoren/intel-socket-1155-cpus.html","processor"
-processor2011="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/processoren/intel-socket-2011-cpus.html","processor"
-processorFm1="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/processoren/amd-socket-fm1-cpus.html","processor"
-processorFm2="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/processoren/amd-socket-fm2-cpus.html","processor"
-processor1150="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/processoren/intel-socket-1150-cpus.html","processor"
-videokaartAMD="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/videokaarten/videokaarten-amd-chipset.html","grafischekaart"
-videokaartNVIDIA="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/videokaarten/videokaarten-nvidia-chipset.html","grafischekaart"
-voeding="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/voedingen/pc-voeding.html","voeding"
 
+#product urls moederborden
+mbMiniITX="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/moederborden/mini-itx-moederborden.html","Moederbord"
+mbSocket1150="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/moederborden/socket-1150-moederborden.html","Moederbord"
+mbSocket1155="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/moederborden/socket-1155-moederborden.html","Moederbord"
+mbSocket2011="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/moederborden/socket-2011-intel-moederborden.html","Moederbord"
+mbSocket478="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/moederborden/socket-478-moederborden.html","Moederbord"
+mbSocketAM="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/moederborden/socket-am3-amd-moederborden.html","Moederbord"
+mbSocketFM="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/moederborden/socket-fm2-amd-moederborden.html","Moederbord"
+
+#product urls processoren
+processorAMD = "https://www.conrad.nl/nl/computer-kantoor/pc-componenten/processoren/amd-socket-am3-cpus.html","Processor"
+processor1155="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/processoren/intel-socket-1155-cpus.html","Processor"
+processor2011="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/processoren/intel-socket-2011-cpus.html","Processor"
+processorFm1="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/processoren/amd-socket-fm1-cpus.html","Processor"
+processorFm2="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/processoren/amd-socket-fm2-cpus.html","Processor"
+processor1150="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/processoren/intel-socket-1150-cpus.html","Processor"
+
+
+#product urls overige producten
+geluidskaart="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/multimedia-hardware/geluidskaarten-intern.html","Geluidskaart"
+behuizing="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/pc-behuizingen/pc-behuizingen.html","Behuizing"
+videokaartAMD="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/videokaarten/videokaarten-amd-chipset.html","Grafischekaart"
+videokaartNVIDIA="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/videokaarten/videokaarten-nvidia-chipset.html","Grafischekaart"
+voeding="https://www.conrad.nl/nl/computer-kantoor/pc-componenten/voedingen/pc-voeding.html","Voeding"
+
+# 2 arrays voor de urls en de label namen.
 urlsOverview = []
 namesOverview = []
 
+#create a connecting with the database on our server
+print ("Connecting with database")
 graph_db = neo4j.GraphDatabaseService("http://145.24.222.101:8001/db/data/")
 batch = neo4j.WriteBatch(graph_db)
-#http://145.24.222.101:8001/db/data/
-#http://localhost:7474/db/data/
 
 
+#The crawl function
 def getInfo():
-    col = []
     i=0
 
+# for each url in the urloverview the crawler load de page and start searching for the div's we ask for.
+    try:
+        print ("Connecting Conrad.nl")
+        for url in urlsOverview:
+            if i == 0:
+                print ("job running")
 
-    for url in urlsOverview:
-        webpage = urllib2.urlopen(url)
-        soup = BeautifulSoup(webpage)
+            webpage = urllib2.urlopen(url)
+            soup = BeautifulSoup(webpage)
 
 
-        for item in soup.findAll("div", {"class" : "c-searchresult-list-item-details-inner"}):
-            producturlinput = item.find("a",{"class" : "c-searchresult-list-item-link"}).get('href')
-            producturl = "https://www.conrad.nl/"+producturlinput
+            for item in soup.findAll("div", {"class" : "c-searchresult-list-item-details-inner"}):
+                producturlinput = item.find("a",{"class" : "c-searchresult-list-item-link"}).get('href')
+                producturl = "https://www.conrad.nl/"+producturlinput
 
-            detailpagina = urllib2.urlopen(producturl)
-            detailsoup = BeautifulSoup(detailpagina)
-            name =str(detailsoup.find("h1", {"class": "c-productdetails-title"}).getText())
-            price = str(detailsoup.find("div",{"class":"product-price"}).getText()).replace(u"\u20ac", '')
-            info = []
-            names = []
-            table= detailsoup.findAll("table" , {"class": "c-productdetails-specifications-table"})
-            dict = {"Name":name, "ProductUrl": producturl}
-            #newNode = batch.create(node({"Name": name, "ProductUrl": producturl}))
+                detailpagina = urllib2.urlopen(producturl)
+                detailsoup = BeautifulSoup(detailpagina)
+                name =str(detailsoup.find("h1", {"class": "c-productdetails-title"}).getText())
+                price = str(detailsoup.find("div",{"class":"product-price"}).getText()).replace(u"\u20ac", '').replace(" ", '').strip('\t\r\n\xa0')
 
-            for rownames in detailsoup.findAll("tr"):
-                try:
-                    rname = str(rownames.find("td" , {"class": "column-left"}).getText()).replace(":","")
-                except:
-                    rname="onbekend"
-                try:
-                    rinfo = str(rownames.find("td" , {"class":"column-right"}).getText()).replace("`","")
-                except:
-                    rinfo = "onbekend"
-                dicttemp = {rname:rinfo}
-                dict.update(dicttemp)
+                # the itema above we use as properties in the neo4j database.
+                # For creating a node we need to create a dictonaire below.
+                dict = {"Name":name, "ProductUrl": producturl,"Price":price}
 
-            newNode = batch.create(node((dict)))
-            #newNode, =graph_db.create(node({dict}))
-            label= str(namesOverview[i])
-            batch.add_labels(newNode,label)
+                for rownames in detailsoup.findAll("tr"):
+                    try:
 
-            print (label)
-            print (name)
-            print (price)
-            print (producturl)
-        i=i+1
+                        rname = str(rownames.find("td" , {"class": "column-left"}).getText()).replace(":","").split(" ").lower()
+                    except:
+                        rname="onbekend"
+                    try:
+                        rinfo = str(rownames.find("td" , {"class":"column-right"}).getText()).replace(":","").strip('\t\r\n\xa0').lower()
+                        rinfoParsingStorage=''
+                        for letter in rinfo:
+                            if letter.isalpha() or letter.isdigit():
+                                rinfoParsingStorage += letter
+
+                    except:
+                        rinfoParsingStorage = "onbekend"
+
+
+                    dicttemp = {rname[0]:rinfoParsingStorage}
+                    dict.update(dicttemp)
+
+                #Creating the node
+                newNode = batch.create(node((dict)))
+
+                #add label to the node
+                label= str(namesOverview[i])
+                batch.add_labels(newNode,label)
+
+            percentage = (100/len(urlsOverview) *(i+1))
+            procces = "Percentage : "+str("%.2f" % percentage)+"% done"
+            print (procces)
+            i=i+1
+    except:
+        print("connection problem")
 
 
 
@@ -165,7 +193,7 @@ def createLabelNames():
     namesOverview.append(mbSocket478[1])
     namesOverview.append(mbSocketAM[1])
     namesOverview.append(mbSocketFM[1])
-    namesOverview.append(geluidskaart[0])
+    namesOverview.append(geluidskaart[1])
     namesOverview.append(behuizing[1])
     namesOverview.append(processorAMD[1])
     namesOverview.append(processor1155[1])
