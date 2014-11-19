3a04f01f7cf8f30c8a1cda952f61a4dfc56331cd
Razmaklat <olsonyarzagaray@outlook.com>
2014-11-19 22:03:57 +0100

    First Commit Matcher

---
+++ MatcherPrototype/Components.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatcherPrototype
+{
+    class Components
+    {
+        private string naam { get; set; }
+        private int price { get; set; }
+        private string url { get; set; }
+        private string beschrijving { get; set; }
+        private string bedrijfsnaam { get; set; }
+    }
+
+    class Moederbord : Components
+    {
+        private string socket { get; set; }
+        private string vormFactor { get; set; }
+        private int geheugenslot { get; set; }
+        private string geheugenkloksneleheid { get; set; }
+        private string geheugentype { get; set; }
+        private int maxgeheugen { get; set; }
+        private string aansluiting { get; set;}
+    }
+
+    class Geheugen : Components
+    {
+        private string geheugenkloksnelehid { get; set; }
+        private string geheugentype { get; set; }
+        private int geheugen { get; set; }
+        private int aantal { get; set; }
+    }
+
+    class CPUKoeler : Components 
+    {
+        private string socket { get; set; }
+    }
+
+    class Voeding : Components
+    {
+        private int wattage { get; set; }
+    }
+
+    class CPU : Components
+    {
+        private string socket { get; set; }
+        private int kloksnelheid { get; set; }
+        private int cores { get; set; }
+    }
+
+    class GrafischeKaart : Components
+    {
+        private string aansluiting { get; set; }
+        private int videoGeheugen { get; set; }
+        private string typeGeheugen { get; set; }
+    }
+
+    class Behuizing : Components
+    {
+        private string vormFactor { get; set; }
+    }
+
+    class Hardeschijf : Components
+    {
+        private string soort { get; set; }
+        private int opslag { get; set; }
+        private string rpm { get; set;}
+    }
+
+    class OptischeDrives : Components
+    {
+        private string categorie { get; set; }
+    }
+}
---
+++ MatcherPrototype/QueryManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatcherPrototype
+{
+    class QueryManager
+    {
+        private GraphClient client;
+
+        private void initClientConnection()
+        {
+            try
+            {
+                if (this.client != null)
+                {
+                    return;
+                }
+
+                this.client = new GraphClient(new Uri("http://localhost:7474/db/data"));
+                this.client.Connect();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Could not make connection to database, check if database server is on and try again.");
+            }
+
+
+        }
+    }
+}
