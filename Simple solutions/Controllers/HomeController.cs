4d4dc41cd930deace7aec2e111166ae8ee4c9fd7
Geddy05 <geddyschellevis@gmail.com>
2014-12-12 14:28:12 +0100

    matcher+ front end

---
+++ Simple solutions/Controllers/HomeController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Simple_solutions.Models;
+
+
+namespace Simple_solutions.Matcher
+{
+    public class HomeController : Controller
+    {
+        private MatcherMainProgram prog;
+
+        public ActionResult Index()
+        {
+            List<SelectListItem> processortype = new List<SelectListItem>();
+            processortype.Add(new SelectListItem { Text = "", Value = "i3" });
+            processortype.Add(new SelectListItem { Text = "i3", Value = "i3" });
+            processortype.Add(new SelectListItem { Text = "i5", Value = "i5" });
+            processortype.Add(new SelectListItem { Text = "i7", Value = "i7" });
+            processortype.Add(new SelectListItem { Text = "g3220", Value = "g3220" });
+            processortype.Add(new SelectListItem { Text = "AMD fx", Value = "fx" });
+            processortype.Add(new SelectListItem { Text = "AMD a10", Value = "a10" });
+            processortype.Add(new SelectListItem { Text = "AMD a4", Value = "a4" });
+            processortype.Add(new SelectListItem { Text = "AMD a8", Value = "a8" });
+            ViewData["processortypeDD"] = processortype;
+            
+            List<SelectListItem> processorsnelheid = new List<SelectListItem>();
+            processorsnelheid.Add(new SelectListItem { Text = "", Value = "1 - 2" });
+            processorsnelheid.Add(new SelectListItem { Text = "1-2 GHZ", Value = "1 - 2" });
+            processorsnelheid.Add(new SelectListItem { Text = "2-3 GHZ", Value = "2 - 3" });
+            processorsnelheid.Add(new SelectListItem { Text = "3-4 GHZ", Value = "3 - 4" });
+            processorsnelheid.Add(new SelectListItem { Text = "4-5 GHZ", Value = "4 - 5" });
+            ViewData["processorsnelheidDD"] = processorsnelheid;
+
+            
+            List<SelectListItem> processorcores = new List<SelectListItem>();
+            processorcores.Add(new SelectListItem { Text = "", Value = "1" });
+            processorcores.Add(new SelectListItem { Text = "1", Value = "1" });
+            processorcores.Add(new SelectListItem { Text = "2", Value = "2" });
+            processorcores.Add(new SelectListItem { Text = "3", Value = "3" });
+            processorcores.Add(new SelectListItem { Text = "4", Value = "4" });
+            processorcores.Add(new SelectListItem { Text = "6", Value = "6" });
+            processorcores.Add(new SelectListItem { Text = "8", Value ="8" });
+            processorcores.Add(new SelectListItem { Text = "12", Value = "12" });
+            ViewData["processorcoresDD"] = processorcores;
+
+            List<SelectListItem> geheugenslots = new List<SelectListItem>();
+            geheugenslots.Add(new SelectListItem { Text = "", Value = "2" });
+            geheugenslots.Add(new SelectListItem { Text = "2", Value = "2" });
+            geheugenslots.Add(new SelectListItem { Text = "4", Value = "4" });
+            geheugenslots.Add(new SelectListItem { Text = "6", Value = "6" });
+            geheugenslots.Add(new SelectListItem { Text = "8", Value = "8" });
+            ViewData["geheugenslotsDD"] = geheugenslots;
+
+            List<SelectListItem> geheugensnelheid = new List<SelectListItem>();
+            geheugensnelheid.Add(new SelectListItem { Text = "", Value = "1" });
+            geheugensnelheid.Add(new SelectListItem { Text = "1 gb", Value = "1" });
+            geheugensnelheid.Add(new SelectListItem { Text = "2 gb", Value = "2" });
+            geheugensnelheid.Add(new SelectListItem { Text = "4 gb", Value = "4" });
+            geheugensnelheid.Add(new SelectListItem { Text = "8 gb", Value = "8" });
+            geheugensnelheid.Add(new SelectListItem { Text = "16 gb", Value = "16" });
+            geheugensnelheid.Add(new SelectListItem { Text = "32 gb", Value = "32" });
+            ViewData["geheugensnelheidDD"] = geheugensnelheid;
+
+            List<SelectListItem> grafischekaartvideogeheugen = new List<SelectListItem>();
+            grafischekaartvideogeheugen.Add(new SelectListItem { Text = "", Value = "1gb" });
+            grafischekaartvideogeheugen.Add(new SelectListItem { Text = "1gb", Value = "1gb" });
+            grafischekaartvideogeheugen.Add(new SelectListItem { Text = "2gb", Value = "2gb" });
+            grafischekaartvideogeheugen.Add(new SelectListItem { Text = "4gb", Value = "4gb" });
+            grafischekaartvideogeheugen.Add(new SelectListItem { Text = "8gb", Value = "8gb" });
+            grafischekaartvideogeheugen.Add(new SelectListItem { Text = "12gb", Value = "12gb" });
+            ViewData["grafischekaartvideogeheugenDD"] = grafischekaartvideogeheugen;
+
+            List<SelectListItem> grafischekaarttype = new List<SelectListItem>();
+            grafischekaarttype.Add(new SelectListItem { Text = "", Value = "ddr2" });
+            grafischekaarttype.Add(new SelectListItem { Text = "ddr", Value = "ddr" });
+            grafischekaarttype.Add(new SelectListItem { Text = "ddr2", Value = "ddr2" });
+            grafischekaarttype.Add(new SelectListItem { Text = "ddr3", Value = "ddr3" });
+            grafischekaarttype.Add(new SelectListItem { Text = "gddr3", Value = "gddr3" });
+            grafischekaarttype.Add(new SelectListItem { Text = "gddr5", Value = "gddr5" });
+            ViewData["grafischekaarttypeDD"] = grafischekaarttype;
+
+            List<SelectListItem> hardeschijftype = new List<SelectListItem>();
+            hardeschijftype.Add(new SelectListItem { Text = "", Value = "hdd" });
+            hardeschijftype.Add(new SelectListItem { Text = "hdd", Value = "hdd" });
+            hardeschijftype.Add(new SelectListItem { Text = "ssd", Value = "ssd" });
+            ViewData["hardeschijftypeDD"] = hardeschijftype;
+
+            List<SelectListItem> hardeschijfopslag = new List<SelectListItem>();
+            hardeschijfopslag.Add(new SelectListItem { Text = "", Value = "50 - 100" });
+            hardeschijfopslag.Add(new SelectListItem { Text = "50-100 GB", Value = "50 - 100" });
+            hardeschijfopslag.Add(new SelectListItem { Text = "100-200 GB", Value = "100 - 200" });
+            hardeschijfopslag.Add(new SelectListItem { Text = "200-400 GB", Value = "200 - 400" });
+            hardeschijfopslag.Add(new SelectListItem { Text = "400-1000 GB", Value = "400 - 1000" });
+            hardeschijfopslag.Add(new SelectListItem { Text = "1000-2000 GB", Value = "1000 - 2000" });
+            ViewData["hardeschijfopslagDD"] = hardeschijfopslag;
+
+            List<SelectListItem> optischedrivescategorie = new List<SelectListItem>();
+            optischedrivescategorie.Add(new SelectListItem { Text = "", Value = "0" });
+            optischedrivescategorie.Add(new SelectListItem { Text = "BLU-RAY", Value = "blu-ray" });
+            optischedrivescategorie.Add(new SelectListItem { Text = "DVD RW", Value = "dvd rw" });
+            ViewData["optischedrivescategorieDD"] = optischedrivescategorie;
+
+            List<SelectListItem> behuizingvormfactor = new List<SelectListItem>();
+            behuizingvormfactor.Add(new SelectListItem { Text = "", Value = "0" });
+            behuizingvormfactor.Add(new SelectListItem { Text = "atx", Value = "atx" });
+            behuizingvormfactor.Add(new SelectListItem { Text = "miniatx", Value = "atx" });
+            behuizingvormfactor.Add(new SelectListItem { Text = "200-300 RPM", Value = "200-300 RPM" });
+            ViewData["behuizingvormfactorDD"] = behuizingvormfactor;
+
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string processortypeResult, string processorsnelheidResult, string processorcoresResult, string geheugenslotsResult, string geheugensnelheidResult, string grafischekaartvideogeheugenResult, string grafischekaarttypeResult,
+           string hardeschijftypeResult, string hardeschijfopslagResult, string hardeschijfrpmResult, string optischedrivescategorieResult, string behuizingvormfactorResult)
+        {
+            var searchResults = new SearchPropertiesModel();
+            searchResults.processorType = processortypeResult;
+            searchResults.processorsnelheid = processorsnelheidResult;
+            searchResults.processorcores = processorcoresResult;
+            searchResults.geheugenslots = geheugenslotsResult;
+            searchResults.geheugensnelheid = geheugensnelheidResult;
+            searchResults.grafischekaartvideogeheugen = grafischekaartvideogeheugenResult; 
+            searchResults.grafischekaarttype = grafischekaarttypeResult;
+            searchResults.hardeschijftype = hardeschijftypeResult;
+            searchResults.hardeschijfopslag = hardeschijfopslagResult;
+            searchResults.hardeschijfrpm = hardeschijfrpmResult;
+            searchResults.optischedrivescategorie = optischedrivescategorieResult;
+            searchResults.behuizingvormfactor = behuizingvormfactorResult;
+
+            prog = new MatcherMainProgram(searchResults);
+            prog.runMatcherProgram();
+
+            TempData["results"] = searchResults;
+            return RedirectToAction("About");
+        }
+        
+        public ActionResult About()
+        {
+            var searchResults = TempData["results"] as SearchPropertiesModel;
+            ViewBag.processorType = prog.listNodeProcessor[0].Model;
+            ViewBag.processorsnelheid = "test";
+            ViewBag.processorcores = "test";
+            ViewBag.geheugenslots = "test";
+            ViewBag.geheugensnelheid = "test";
+            ViewBag.grafischekaartvideogeheugen = "test";
+            ViewBag.grafischekaarttype = "test";
+            ViewBag.hardeschijftype = "test";
+            ViewBag.hardeschijfopslag = "test";
+            ViewBag.hardeschijfrpm = "test";
+            ViewBag.optischedrivescategorie = "test";
+            ViewBag.behuizingvormfactor = "test";
+
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Simple solutions/Matcherr/QueryManager.cs
@@ -0,0 +1,356 @@
+﻿using Neo4jClient;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Simple_solutions.Models;
+
+namespace Simple_solutions
+{
+    class QueryManager
+    {
+        private GraphClient client;
+
+        private void initClientConnection()
+        {
+            try
+            {
+                if (this.client != null)
+                {
+                    return;
+                }
+                //http://145.24.222.101:8001/db/data
+                this.client = new GraphClient(new Uri("http://localhost:7474/db/data"));
+                this.client.Connect();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Could not make connection to database, check if database server is on and try again.");
+            }
+
+
+        }
+
+        public void queryMoederbord(Moederbord moederbordNode,CPU processorNode,GeheugenKaart geheugenNode,List<Moederbord> listMoederB)
+        {
+            // Example query to check how the system takes in the nodes and work with it.
+
+            //Create stopwatch to measure performance
+            var watch = Stopwatch.StartNew();
+
+            initClientConnection();
+            var result =
+                this.client.Cypher
+                .Match(" (m:Moederbord)-[a]-(p:Processor), (m)-[c]->(g:Geheugen)")
+                .Where((Moederbord m) => m.Geheugenslots == moederbordNode.Geheugenslots)
+                .AndWhere((CPU p) => p.Model == processorNode.Model && p.Cores == processorNode.Cores)
+                .AndWhere((GeheugenKaart g) => g.Geheugen == geheugenNode.Geheugen)
+                .AndWhere("p.Kloksnelheid >= " + processorNode.MinimumKloksnelheid + " AND p.Kloksnelheid <= " + processorNode.MaximumKloksnelheid)
+                .ReturnDistinct((m) => new
+                {
+                    listM = m.As<Moederbord>(),
+                })
+                .Results;
+                
+
+            foreach(var a in result)
+            {
+                listMoederB.Add(a.listM);
+            }
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("queryMoederbord ElapsedMS: " + elapsedMs);
+        }
+
+        public void queryProcessor(Moederbord moederbordNode,CPU processorNode,GeheugenKaart geheugenNode,List<CPU> listProcessor)
+        {
+            // Example query to check how the system takes in the nodes and work with it.
+
+            //Create stopwatch to measure performance
+            var watch = Stopwatch.StartNew();
+
+            initClientConnection();
+            var result =
+                this.client.Cypher
+                .Match(" (m:Moederbord)-[a]-(p:Processor), (m)-[c]->(g:Geheugen)")
+                .Where((Moederbord m) => m.Geheugenslots == moederbordNode.Geheugenslots)
+                .AndWhere((CPU p) => p.Model == processorNode.Model && p.Cores == processorNode.Cores)
+                .AndWhere((GeheugenKaart g) => g.Geheugen == geheugenNode.Geheugen)
+                .AndWhere("p.Kloksnelheid >= " + processorNode.MinimumKloksnelheid + " AND p.Kloksnelheid <= " + processorNode.MaximumKloksnelheid)
+                .ReturnDistinct((p) => new
+                {
+                    listP = p.As<CPU>(),
+                })
+                .Results;
+
+
+            foreach (var a in result)
+            {
+                listProcessor.Add(a.listP);
+            }
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("queryProcessor ElapsedMS: " + elapsedMs);
+        }
+
+        public void queryGeheugenKaart(Moederbord moederbordNode,CPU processorNode,GeheugenKaart geheugenNode,List<GeheugenKaart> listGeheugenkaart)
+        {
+            // Example query to check how the system takes in the nodes and work with it.
+
+            //Create stopwatch to measure performance
+            var watch = Stopwatch.StartNew();
+
+            initClientConnection();
+            var result =
+                this.client.Cypher
+                .Match(" (m:Moederbord)-[a]-(p:Processor), (m)-[c]->(g:Geheugen)")
+                .Where((Moederbord m) => m.Geheugenslots == moederbordNode.Geheugenslots)
+                .AndWhere((CPU p) => p.Model == processorNode.Model && p.Cores == processorNode.Cores)
+                .AndWhere((GeheugenKaart g) => g.Geheugen == geheugenNode.Geheugen)
+                .AndWhere("p.Kloksnelheid >= " + processorNode.MinimumKloksnelheid + " AND p.Kloksnelheid <= " + processorNode.MaximumKloksnelheid)
+                .ReturnDistinct((g) => new
+                {
+                    listG = g.As<GeheugenKaart>(),
+                })
+                .Results;
+
+
+            foreach (var a in result)
+            {
+                listGeheugenkaart.Add(a.listG);
+            }
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("queryGeheugenKaart ElapsedMS: " + elapsedMs);
+        }
+
+        public void queryOptischedrive(List<Optischedrives> listOptischeDrives)
+        {
+            // Example query to check how the system takes in the nodes and work with it.
+
+            //Create stopwatch to measure performance
+            var watch = Stopwatch.StartNew();
+
+            initClientConnection();
+            var result =
+                this.client.Cypher
+                .Match(" (o:Optischedrives)")
+                .ReturnDistinct((o) => new
+                {
+                    listO = o.As<Optischedrives>(),
+                })
+                .Limit(500)
+                .Results;
+
+
+            foreach (var a in result)
+            {
+                listOptischeDrives.Add(a.listO);
+            }
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("queryOptischedrives ElapsedMS: " + elapsedMs);
+        }
+
+        public void queryHardeschijf(Hardeschijf hardeschijfNode, List<Hardeschijf> listHardeschijf)
+        {
+            // Example query to check how the system takes in the nodes and work with it.
+
+            //Create stopwatch to measure performance
+            var watch = Stopwatch.StartNew();
+
+            initClientConnection();
+            var result =
+                this.client.Cypher
+                .Match(" (h:Hardeschijf)")
+                .Where("h.Opslag >= " + hardeschijfNode.MinimumOpslag + " AND h.Opslag <= " + hardeschijfNode.MaximumOpslag)
+                .ReturnDistinct((h) => new
+                {
+                    listH = h.As<Hardeschijf>(),
+                })
+                .Limit(500)
+                .Results;
+
+
+            foreach (var a in result)
+            {
+                listHardeschijf.Add(a.listH);
+            }
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("queryHardeschijf ElapsedMS: " + elapsedMs);
+        }
+
+        public void queryVoeding(List<Voeding> listVoeding)
+        {
+            // Example query to check how the system takes in the nodes and work with it.
+
+            //Create stopwatch to measure performance
+            var watch = Stopwatch.StartNew();
+
+            initClientConnection();
+            var result =
+                this.client.Cypher
+                .Match(" (v:Voeding)")
+                .ReturnDistinct((v) => new
+                {
+                    listV = v.As<Voeding>(),
+                })
+                .Limit(500)
+                .Results;
+
+
+            foreach (var a in result)
+            {
+                listVoeding.Add(a.listV);
+            }
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("queryVoeding ElapsedMS: " + elapsedMs);
+        }
+
+        public void queryGrafischekaart(GrafischeKaart grafischekaartNode, List<GrafischeKaart> listGrafischeKaart)
+        {
+            // Example query to check how the system takes in the nodes and work with it.
+
+            //Create stopwatch to measure performance
+            var watch = Stopwatch.StartNew();
+
+            initClientConnection();
+            var result =
+                this.client.Cypher
+                .Match(" (g:Grafischekaart)")
+                .Where((GrafischeKaart g) => g.Typegeheugen == grafischekaartNode.Typegeheugen && g.Videogeheugen == grafischekaartNode.Videogeheugen )
+                .ReturnDistinct((g) => new
+                {
+                    listG = g.As<GrafischeKaart>(),
+                })
+                .Limit(500)
+                .Results;
+
+
+            foreach (var a in result)
+            {
+                listGrafischeKaart.Add(a.listG);
+            }
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("queryGrafischeKaart ElapsedMS: " + elapsedMs);
+        }
+
+        public void queryKoeler(CPU processorNode,List<CPUKoeler> listCPUKoeler)
+        {
+            // Example query to check how the system takes in the nodes and work with it.
+
+            //Create stopwatch to measure performance
+            var watch = Stopwatch.StartNew();
+
+            initClientConnection();
+            var result =
+                this.client.Cypher
+                .Match(" (p:Processor)-[a]-(k:Koeler)")
+                .Where((CPU p) => p.Model == processorNode.Model && p.Cores == processorNode.Cores)
+                .ReturnDistinct((k) => new
+                {
+                    listK = k.As<CPUKoeler>(),
+                })
+                .Limit(500)
+                .Results;
+
+
+            foreach (var a in result)
+            {
+                listCPUKoeler.Add(a.listK);
+            }
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("queryKoeler ElapsedMS: " + elapsedMs);
+        }
+
+        public void queryBehuizing(Moederbord moederbordNode, List<Behuizing> listBehuizing)
+        {
+            // Example query to check how the system takes in the nodes and work with it.
+
+            //Create stopwatch to measure performance
+            var watch = Stopwatch.StartNew();
+
+            initClientConnection();
+            var result =
+                this.client.Cypher
+                .Match(" (m:Moederbord)-[a]-(b:Behuizing)")
+                .Where((Moederbord m) => m.Geheugenslots == moederbordNode.Geheugenslots)
+                .ReturnDistinct((b) => new
+                {
+                    listB = b.As<Behuizing>(),
+                })
+                .Limit(500)
+                .Results;
+
+
+            foreach (var a in result)
+            {
+                listBehuizing.Add(a.listB);
+            }
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("queryBehuizing ElapsedMS: " + elapsedMs);
+        }
+
+        public void runAllQuery(SearchPropertiesModel searchPropertiesModel, List<Moederbord> ListNodeMoederbord, List<CPU> listProcessor, List<GeheugenKaart> listGeheugenkaart, List<Optischedrives> listNOptischedrives,
+            List<Hardeschijf> listHardeschijf, List<GrafischeKaart> listGrafischekaart, List<CPUKoeler> listCPUKoeler,
+            List<Voeding> listVoeding, List<Behuizing> listBehuizing)
+        {
+            //Create stopwatch to measure performance
+            var watch = Stopwatch.StartNew();
+
+            //Create objects to pass in the followings function
+
+            Moederbord moederbordNode = new Moederbord();
+
+
+            CPU processorNode = new CPU();
+            processorNode.Cores = searchPropertiesModel.processorcores;
+            processorNode.Model = searchPropertiesModel.processorType;
+            processorNode.MinimumKloksnelheid = processorNode.setMinimumRange(searchPropertiesModel.processorsnelheid);
+            processorNode.MaximumKloksnelheid = processorNode.setMaximumRange(searchPropertiesModel.processorsnelheid);
+
+            GeheugenKaart geheugenNode = new GeheugenKaart();
+            geheugenNode.Geheugenslots = searchPropertiesModel.geheugenslots;
+            geheugenNode.Geheugenkloksnelheid = searchPropertiesModel.geheugensnelheid;
+
+            Hardeschijf hardeschijfNode = new Hardeschijf();
+            hardeschijfNode.Soort = searchPropertiesModel.hardeschijftype;
+            hardeschijfNode.MinimumOpslag = hardeschijfNode.setMinimumRange(searchPropertiesModel.hardeschijfopslag);
+            hardeschijfNode.MaximumOpslag = hardeschijfNode.setMaximumRange(searchPropertiesModel.hardeschijfopslag);
+
+            GrafischeKaart grafischekaartNode = new GrafischeKaart();
+            grafischekaartNode.Videogeheugen = searchPropertiesModel.grafischekaartvideogeheugen;
+            grafischekaartNode.Typegeheugen = searchPropertiesModel.grafischekaarttype;
+
+            queryMoederbord(moederbordNode, processorNode, geheugenNode, ListNodeMoederbord);
+            queryProcessor(moederbordNode, processorNode, geheugenNode, listProcessor);
+            queryGeheugenKaart(moederbordNode, processorNode, geheugenNode, listGeheugenkaart);
+            queryHardeschijf(hardeschijfNode, listHardeschijf);
+            queryGrafischekaart(grafischekaartNode, listGrafischekaart);
+            queryOptischedrive(listNOptischedrives);
+            queryKoeler(processorNode, listCPUKoeler);
+            queryVoeding(listVoeding);
+            queryBehuizing(moederbordNode, listBehuizing);
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("runAllQuery ElapsedMS: " + elapsedMs);
+        }
+    }
+}
