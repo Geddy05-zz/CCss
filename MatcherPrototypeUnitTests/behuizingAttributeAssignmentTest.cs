01dcf80f38c5b4f18b272e761e9657042e661619
jeroenstravers1 <jeroen.stravers@live.nl>
2015-01-17 22:36:58 +0100

    created Unit Tests, changed Component Classes and Matcher class to public for test accessibility

---
+++ MatcherPrototypeUnitTests/behuizingAttributeAssignmentTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MatcherPrototypeUnitTests
+{
+    [TestClass]
+    public class behuizingAttributeAssignmentTest
+    {
+        [TestMethod]
+        public void testBehuizingAttributeAssignments()
+        {
+            /** assign variables **/
+            MatcherPrototype.Behuizing testBehuizing = new MatcherPrototype.Behuizing()
+            {
+                Naam = "testNaam1 3",
+                Prijs = "testPrijs1 3",
+                Url = "testUrl1 3",
+                Vormfactor = "testVormfactor1 3"
+            };
+
+            /** assert if variables are stored correctly **/
+            Assert.IsInstanceOfType(testBehuizing, typeof(object));
+            Assert.AreEqual(testBehuizing.Naam, "testNaam1 3");
+            Assert.AreEqual(testBehuizing.Prijs, "testPrijs1 3");
+            Assert.AreEqual(testBehuizing.Url, "testUrl1 3");
+            Assert.AreEqual(testBehuizing.Vormfactor, "testVormfactor1 3");
+        }
+    }
+}
