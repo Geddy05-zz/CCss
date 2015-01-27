743162ff38872ecf4e6cc9917551c0cee65901fa
Zammstein <sammeyer1994@hotmail.com>
2015-01-27 22:34:59 +0100

    string error fix try

--- Simple solutions/Controllers/AdminController.cs
+++ Simple solutions/Controllers/AdminController.cs
@@ -32,7 +32,7 @@ namespace Simple_solutions.Controllers
 
             dbHelper = new DBhelper();
             AdminContext db = new AdminContext();
-            query = "SELECT timestamp, AVG(price) as gemiddeldePrijs, type FROM products GROUP BY timestamp, type;";
+            query = "use pcbuilder; SELECT timestamp, AVG(price) as gemiddeldePrijs, type FROM products GROUP BY timestamp, type;";
 
             List<String> columNames = new List<String>(3);
             columNames.Add("timestamp");
@@ -41,8 +41,11 @@ namespace Simple_solutions.Controllers
 
             List<string>[] queryResult = dbHelper.Select(query, 2, columNames);
             ViewData["timestamp"] = queryResult[0];
-            ViewData["gemiddeldePrijs"] = queryResult[1];
-            
+            if (queryResult[1][0] != "") {
+                ViewData["gemiddeldePrijs"] = queryResult[1];
+            } else {
+                ViewData["gemiddeldePrijs"] = queryResult[0];                
+            }
             return View();
         }
     }
