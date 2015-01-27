706ac7780bacfeb808a80977f935585620c37264
Zammstein <sammeyer1994@hotmail.com>
2015-01-27 14:26:33 +0100

    added line chart

--- Simple solutions/Controllers/AdminController.cs
+++ Simple solutions/Controllers/AdminController.cs
@@ -9,28 +9,41 @@ using Simple_solutions.Models;
 using System.Drawing;
 using System.IO;
 using System.Web.Helpers;
+using MySql.Data.MySqlClient;
+using System.Web.UI;
+using System.Collections;
 
 namespace Simple_solutions.Controllers
 {
     public class AdminController : Controller
     {
-
-        //
-        // GET: /Admin/
+        private DBhelper dbHelper;
+        private String query;
 
         public ActionResult Login()
         {
             AdminContext db = new AdminContext();
-            ViewBag.Message = "Test Test";
             return View(db.SearchPropertiesModels.ToList());
         }
 
         public ActionResult Admin()
         {
-             AdminContext db = new AdminContext();
-            ViewBag.Message = "Test Test";
+            ViewBag.PageName = "priceChanges";
+
+            dbHelper = new DBhelper();
+            AdminContext db = new AdminContext();
+            query = "SELECT timestamp, AVG(price) as gemiddeldePrijs, type FROM products GROUP BY timestamp, type;";
+
+            List<String> columNames = new List<String>(3);
+            columNames.Add("timestamp");
+            columNames.Add("gemiddeldePrijs");
+            columNames.Add("type");
+
+            List<string>[] queryResult = dbHelper.Select(query, 2, columNames);
+            ViewData["timestamp"] = queryResult[0];
+            ViewData["gemiddeldePrijs"] = queryResult[1];
+            
             return View();
         }
-
     }
 }
